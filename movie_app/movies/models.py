d8915830d45c7626f2e4c519f082bbc8164af651
ElijahCoding <cuiyeqing960904@gmail.com>
2020-05-23 22:47:26 +0300

    basic movie structure set up, building movie details

--- movie_app/movies/models.py
+++ movie_app/movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 class Category(models.Model):
     """Категории"""
@@ -12,3 +13,62 @@ class Category(models.Model):
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
+
+
+class Actor(models.Model):
+    """Актеры и режиссеры"""
+    name = models.CharField("Имя", max_length=100)
+    age = models.PositiveSmallIntegerField("Возраст", default=0)
+    description = models.TextField("Описание")
+    image = models.ImageField("Изображение", upload_to="actors/")
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Актеры и режиссеры"
+        verbose_name_plural = "Актеры и режиссеры"
+
+class Genre(models.Model):
+    name = models.CharField("Имя", max_length=100)
+    description = models.TextField("Описание")
+    url = models.SlugField(max_length=160, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
+
+class Movie(models.Model):
+    title = models.CharField("Название", max_length=100)
+    tagline = models.CharField("Слоган", max_length=100, default='')
+    description = models.TextField("Описание")
+    poster = models.ImageField("Постер", upload_to="movies/")
+    year = models.PositiveSmallIntegerField("Дата выхода", default=2019)
+    country = models.CharField("Страна", max_length=30)
+    directors = models.ManyToManyField(Actor, verbose_name="режиссер", related_name="film_director")
+    actors = models.ManyToManyField(Actor, verbose_name="актеры", related_name="film_actor")
+    genres = models.ManyToManyField(Genre, verbose_name="жанры")
+    world_premiere = models.DateField("Примьера в мире", default=date.today)
+    budget = models.PositiveIntegerField("Бюджет", default=0,
+                                         help_text="указывать сумму в долларах")
+    fees_in_usa = models.PositiveIntegerField(
+        "Сборы в США", default=0, help_text="указывать сумму в долларах"
+    )
+    fees_in_world = models.PositiveIntegerField(
+        "Сборы в мире", default=0, help_text="указывать сумму в долларах"
+    )
+    category = models.ForeignKey(
+        Category, verbose_name="Категория", on_delete=models.SET_NULL, null=True
+    )
+    url = models.SlugField(max_length=130, unique=True)
+    draft = models.BooleanField("Черновик", default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Фильм"
+        verbose_name_plural = "Фильмы"
