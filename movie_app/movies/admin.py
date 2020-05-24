e9f963ded222dcef37c182071b0d0510f1c17256
ElijahCoding <cuiyeqing960904@gmail.com>
2020-05-24 21:59:13 +0300

    rendering images in admin

--- movie_app/movies/admin.py
+++ movie_app/movies/admin.py
@@ -18,22 +18,33 @@ class ReviewInline(admin.TabularInline):
     extra = 1
     readonly_fields = ("name", "email")
 
+class MovieShotsInline(admin.TabularInline):
+    model = MovieShots
+    extra = 1
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(f"<img src={obj.image.url} width='50' height='60' />")
+
+    get_image.short_description = "Изображение"
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     """Фильмы"""
     list_display = ("title", "category", "url", "draft")
     list_filter = ("category", "year")
     search_fields = ("title", "category__name")
-    inlines = [ReviewInline]
+    inlines = [MovieShotsInline, ReviewInline]
     save_on_top = True
     save_as = True
     list_editable = ("draft",)
+    readonly_fields = ('get_image',)
     fieldsets = (
         (None, {
             "fields": (("title", "tagline"),)
         }),
         (None, {
-            "fields": ("description", "poster")
+            "fields": ("description", "poster", "get_image")
         }),
         (None, {
             "fields": (("year", "world_premiere", "country"),)
@@ -49,6 +60,11 @@ class MovieAdmin(admin.ModelAdmin):
             "fields": (("url", "draft"),)
         }),
     )
+
+    def get_image(self, obj):
+        return mark_safe(f"<img src={obj.poster.url} width='50' height='60' />")
+
+    get_image.short_description = "Изображение"
 
 @admin.register(Reviews)
 class ReviewAdmin(admin.ModelAdmin):
@@ -86,3 +102,5 @@ class MovieShotsAdmin(admin.ModelAdmin):
     list_display = ("title", "movie")
 
 admin.site.register(RatingStar)
+admin.site.site_title = "Django Movies"
+admin.site.site_header = "Django Movies"
