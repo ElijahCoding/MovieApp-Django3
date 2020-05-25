317d251978cf5429c65084d6b9b87937f004166d
ElijahCoding <cuiyeqing960904@gmail.com>
2020-05-25 23:57:14 +0300

    added ckeditor with youtube plugin

--- movie_app/movies/urls.py
+++ movie_app/movies/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("", views.MoviesView.as_view()),
     path("<slug:slug>/", views.MovieDetailView.as_view(), name='movie_detail'),
     path("review/<int:pk>/", views.AddReview.as_view(), name='add_review'),
+
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
