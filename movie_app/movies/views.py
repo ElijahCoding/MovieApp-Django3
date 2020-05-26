0f49299fcccd78a10db9724953c645206280f8a0
ElijahCoding <cuiyeqing960904@gmail.com>
2020-05-26 21:39:17 +0300

    rendering template tag with last movies

--- movie_app/movies/views.py
+++ movie_app/movies/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
-from .models import Movie
+from .models import Movie, Category
 from .forms import ReviewForm
 
 class MoviesView(ListView):
     model = Movie
     queryset = Movie.objects.filter(draft=False)
     template_name = 'movies/movie_list.html'
-
 
 class MovieDetailView(DetailView):
     model = Movie
